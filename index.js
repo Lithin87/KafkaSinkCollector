54a19aeb361a708d3e7be3a63f5d2016e22cb0ca
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2023-08-12 22:30:39 +0530

    added creds & firestore doc

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const express = require('express');
+const { Firestore } = require('@google-cloud/firestore');
 
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+// Replace 'path/to/your/credentials.json' with the actual path to your credentials file
+const firestore = new Firestore({
+  projectId: 'ecstatic-cosmos-387220',
+  keyFilename: './Resources/gcloud.json',
 });
 
-app.post('/', (req, res) => {
-  console.warn(req.body);
-res.send('Hello, Express!');
-});
+async function addDocument() {
+  const docRef = firestore.collection('users').doc('new-user-id');
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  await docRef.set({
+    name: 'John Doe',
+    email: 'john@example.com',
+  });
+
+  console.log('Document added!');
+}
+
+addDocument();
+
+
+// const app = express();
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// app.post('/', (req, res) => {
+//   console.warn(req.body);
+// res.send('Hello, Express!');
+// });
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
  

c759323b722eb2493f32c25d2f9ff368f09bea1d
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2023-08-12 19:17:20 +0530

    added post

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
-// Define a route
+
 app.get('/', (req, res) => {
-    console.warn(req.body);
   res.send('Hello, Express!');
 });
 
+app.get('/', (req, res) => {
+  console.warn(req.body);
+res.send('Hello, Express!');
+});
 
 const port = 3000;
 app.listen(port, () => {

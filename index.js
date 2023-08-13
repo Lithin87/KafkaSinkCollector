7242f1a365a7b6151e6921c945488ed5eea18005
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2023-08-13 14:27:43 +0530

    .

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ addDocument = record => docRef1.add( record).then((docRef) => {
 });
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
